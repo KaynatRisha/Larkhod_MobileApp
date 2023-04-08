738dace2c34425c6948d66164d982b938fc11cb5
KaynatRisha <kaynat.risha10@gmail.com>
2023-04-08 09:22:48 +0430

    structed added

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View } from 'react-native';
+import ViewImageScreen from "./App/screens/ViewImageScreen";
+import welcomeScreen from "./App/screens/welcomeScreen";
+import {MaterialCommunityIcons} from '@expo/vector-icons';
+import AppText from "./App/components/AppText ";
+export default function App() {
+  return (
+  <welcomeScreen />
+  );
+  //<welcomeScreen />
+ // <ViewImageScreen />
+  
+}
+
